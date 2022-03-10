fa88a6d806f73336ea5bee27ff03ca347b0f8107
kaushik867 <kaushikk867@gmail.com>
2022-03-10 17:57:18 +0530

    Simple http server with some static code

--- index.js
+++ index.js
@@ -1,11 +1,38 @@
-const express = require('express')
-const app = express()
-const port = 3000
+const http = require('http');
+const { kill } = require('process');
 
-app.get('/', (req, res) => {
-  res.send('Hello World!!!')
-})
+const server = http.createServer((req, res) => {
+  if(req.url === '/') {
+    res.setHeader('Content-Type', 'text/html');
+    res.write('<html>');
+    res.write('<head><title>First Page</title></head>');
+    res.write('<body><form action="/message" method="POST"><input type="text" name="message"><button type="submit">submit</button></form></body>');
+    res.write('</html>');
+    return res.end();
+  }
+  if(req.url === '/message' && req.method === 'POST') {
+    const body = [];
+    req.on('data', (chunk) => {
+      console.log(chunk);
+      body.push(chunk);
+    });
 
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+    return req.on('end', () => {
+      const parseBody = Buffer.concat(body).toString();
+      console.log(parseBody);
+      res.statusCode = 302;
+      res.setHeader('Location', '/');
+      return res.end();
+    });
+   
+  }
+
+    res.setHeader('Content-Type', 'text/html');
+    res.write('<html>');
+    res.write('<head><title>First Page</title></head>');
+    res.write('<body>Hello from NodeJS</body>');
+    res.write('</html>');
+    return res.end();
+});
+
+server.listen(3000);
